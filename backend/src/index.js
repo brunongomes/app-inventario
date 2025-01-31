1688bbd953cb72b4dbb7e4826de2d064b7924c00
Bruno Gomes <brunongomes8@gmail.com>
2025-01-31 13:19:07 -0300

    feat: enable cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,13 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const itemRoutes = require('./routes/itemRoutes');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 connectDB();
 
