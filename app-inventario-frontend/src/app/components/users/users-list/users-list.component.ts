af75bded83726f753e662a6d33818de9bebe914b
Bruno Gomes <brunongomes8@gmail.com>
2025-02-12 15:38:58 -0300

    feat: implement user actions

--- app-inventario-frontend/src/app/components/users/users-list/users-list.component.ts
+++ app-inventario-frontend/src/app/components/users/users-list/users-list.component.ts
@@ -5,29 +5,31 @@ import { UsersService } from '../../../services/users.service';
 import { Users } from '../../../shared/interfaces/users';
 import { UsersModalComponent } from '../users-modal/users-modal.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
   imports: [
     MenuComponent,
-    MatTableModule
+    MatTableModule,
+    MatIconModule
   ],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent {
-  displayedColumns: string[] = ['name', 'email', 'type'];
+  displayedColumns: string[] = ['name', 'email', 'type', 'actions'];
   dataSource: Users[] = [];
 
 
   constructor(private usersService: UsersService, private dialog: MatDialog) { }
 
   ngOnInit() {
-    this.loadItems();
+    this.loadUsers();
   }
 
-  loadItems() {
+  loadUsers() {
     this.usersService.getUsers().subscribe(users => {
       this.dataSource = users.map((user: { _id: string; nome: string; email: string; tipo: string; }) => {
         return {
@@ -49,8 +51,31 @@ export class UsersListComponent {
     dialogRef.afterClosed().subscribe(result => {
       console.log(result)
       this.usersService.createUser(result.user).subscribe(() => {
-        this.loadItems();
+        this.loadUsers();
       });  
+    });
+  };
+
+  openViewUserDialog(user: Users) {
+    const dialogRef = this.dialog.open(UsersModalComponent, {
+      width: '500px',
+      data: { user: user, isNewUser: false }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('===>', result)
+      
+      if (result.action === 'save') {
+        this.usersService.updateUser(result.user).subscribe(() => {
+          this.loadUsers();
+        });
+      }
+
+      if (result.action === 'delete') {
+        this.usersService.deleteUser(result.user.id).subscribe(() => {
+          this.loadUsers();
+        });
+      }
     });
   }
 }
