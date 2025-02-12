a4a41e92f20d7e09d6fc0fa2ed8c11c2d1ee6052
Bruno Gomes <brunongomes8@gmail.com>
2025-02-12 15:47:06 -0300

    feat: implement menu change

--- app-inventario-frontend/src/app/shared/menu/menu.component.ts
+++ app-inventario-frontend/src/app/shared/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -9,5 +10,26 @@ import {MatTabsModule} from '@angular/material/tabs';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  selectedTabIndex = 0;
 
+  constructor(private router: Router) {}
+
+  onTabChange(event: any): void {
+    this.selectedTabIndex = event.index;
+
+    if (event.index === 0) {
+      this.router.navigate(['/itens']);
+    } else if (event.index === 1) {
+      this.router.navigate(['/usuarios']);
+    }
+  }
+
+  ngOnInit(): void {
+    const currentRoute = this.router.url;
+    if (currentRoute.includes('/itens')) {
+      this.selectedTabIndex = 0;
+    } else if (currentRoute.includes('/usuarios')) {
+      this.selectedTabIndex = 1;
+    }
+  }
 }
